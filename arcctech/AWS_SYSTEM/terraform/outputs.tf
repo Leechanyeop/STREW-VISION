33445a4dd3a5c62d4a057bb8b67e7bceb7cf1981
Leechanyeop <69401287+Leechanyeop@users.noreply.github.com>
2026-06-30 23:21:43 +0900

    20260630 Rev.1
    6/30

--- arcctech/AWS_SYSTEM/terraform/outputs.tf
+++
@@ -1,2 +0,0 @@
-﻿output "api_url" { value = "http://${aws_lb.api.dns_name}" }
-output "dynamodb_table" { value = aws_dynamodb_table.events.name }
--- arcctech/AWS_SYSTEM/terraform/variables.tf
+++
@@ -1,4 +0,0 @@
-﻿variable "project_name" { default = "strew-vision" }
-variable "aws_region" { default = "ap-northeast-2" }
-variable "container_image" { description = "ECR image URI" }
-variable "api_key" { description = "API key shared by dashboard and Jetson" sensitive = true }
